4208f06b6d715c755be2b6ff16d468f407e64ff5
lombards <andrea@linux.com>
2019-02-10 16:33:56 +0000

    Implement drop and dropwhile

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,13 +50,34 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => throw new IllegalStateException()
+      case Cons(_, xs) => xs
+    }
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = {
+    l match {
+      case Nil => throw new IllegalStateException()
+      case Cons(_, xs) => Cons(h, xs)
+    }
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, xs) => drop(xs, n-1)
+    }
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match{
+      case Nil => Nil
+      case Cons(x,xs) => if (f(x)) dropWhile(xs,f) else l
+    }
+  }
 
   def init[A](l: List[A]): List[A] = ???
 
